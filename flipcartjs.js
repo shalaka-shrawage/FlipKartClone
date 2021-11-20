81f8e7ba3d045b247a2cf92490d9a4a377c4815b
shalaka-shrawage <84834211+shalaka-shrawage@users.noreply.github.com>
2021-11-20 13:34:12 +0530

    Add files via upload

---
+++ flipcartjs.js
@@ -0,0 +1,13 @@
+document. getElementById("loginid"). onclick = function () {
+    location. href = "file:///D:/AccioJobs/Flipcart/loginhtml.html";
+    };
+
+
+    document. getElementById("logincart"). onclick = function () {
+        location. href = "    file:///D:/AccioJobs/Flipcart/carthtml.html";
+        };
+        
+
+        document. getElementById("logincartbtn"). onclick = function () {
+            location. href = "    file:///D:/AccioJobs/Flipcart/carthtml.html";
+            };
